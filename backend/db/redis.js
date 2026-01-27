61aa8a28edce3a69d3a8af0c7fdf8cb088c92a87
Uday5277 <udayp5277@gmail.com>
2026-01-27 22:30:55 +0530

    Update folder structure

--- backend/db/redis.js
+++ backend/db/redis.js
@@ -4,7 +4,7 @@ import path from "path";
 
 dotenv.config({ path: path.resolve(process.cwd(), '.env') }); 
 
-console.log("Connecting to Redis at:", process.env.REDIS_URL); 
+console.log("👉Connecting to Redis at:", process.env.REDIS_URL); 
 
 const redis = createClient({
   url: process.env.REDIS_URL,
